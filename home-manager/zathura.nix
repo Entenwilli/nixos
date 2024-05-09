5af68e0efcde5dba32465fe64c31eea4bc05d57a
Felix Schwickerath <felix@fschwickerath.de>
2024-05-09 20:59:30 +0200

    feat(zathura): tell zathura to use system clipboard

--- home-manager/zathura.nix
+++ home-manager/zathura.nix
@@ -10,6 +10,9 @@
   config = lib.mkIf config.zathura.enable {
     programs.zathura = {
       enable = true;
+      extraConfig = ''
+        set selection-clipboard clipboard
+      '';
     };
   };
 }
