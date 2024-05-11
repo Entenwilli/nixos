c28fd9f800d38c164b1a955ae42d837c8e593545
Felix Schwickerath <felix@fschwickerath.de>
2024-05-11 22:05:00 +0200

    feat(dfa): update data flow analysis to new version

--- pkgs/eclipse-dfa/default.nix
+++ pkgs/eclipse-dfa/default.nix
@@ -1,51 +1,88 @@
-{ lib, stdenv, makeDesktopItem, freetype, fontconfig, libX11, libXrender
-, zlib, jdk, glib, glib-networking, gtk3, libXtst, libsecret, gsettings-desktop-schemas, webkitgtk
-, makeWrapper, libarchive, fetchurl, ... }:
-
+{
+  lib,
+  stdenv,
+  makeDesktopItem,
+  freetype,
+  fontconfig,
+  libarchive,
+  libX11,
+  libXrender,
+  perl,
+  zlib,
+  jdk,
+  glib,
+  glib-networking,
+  gtk3,
+  libXtst,
+  libsecret,
+  gsettings-desktop-schemas,
+  webkitgtk,
+  makeWrapper,
+  fetchurl,
+  ...
+}:
 stdenv.mkDerivation rec {
   name = "eclipse-dfa";
+  version = "2.0.0";
   desktopItem = makeDesktopItem {
     name = "DataFlowAnalysis";
-    exec = "eclipse";
-    icon = "eclipse";
+    exec = "DataFlowAnalysisBench";
+    icon = "DataFlowAnalysisBench";
     comment = "Integrated Development Environment";
     desktopName = "DataFlowAnalysis";
     genericName = "Integrated Development Environment";
-    categories = [ "Development" ];
-  }; 
+    categories = ["Development"];
+  };
 
-  src = 
-    fetchurl {
-      url = "https://github.com/DataFlowAnalysis/DataFlowAnalysis/releases/download/v1.0.0/DataFlowAnalysis.linux.gtk.x86_64.zip";
-      hash = "sha256-JRXVuck5JCr6ox4/siK02mMBQgsGbS5mQjX2NiWsefw=";
-    };
+  src = fetchurl {
+    url = "https://github.com/DataFlowAnalysis/DataFlowAnalysis/releases/download/v${version}/DataFlowAnalysis.linux.gtk.x86_64.zip";
+    hash = "sha256-6P8wxhdnwMGDBWKWC4DsDfCRVp+T/ApN5oRwO94dldE=";
+  };
 
- nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [makeWrapper perl];
   buildInputs = [
-    fontconfig freetype glib gsettings-desktop-schemas gtk3 jdk libX11
-    libXrender libXtst libsecret zlib libarchive
-  ] ++ lib.optional (webkitgtk != null) webkitgtk;
+    fontconfig
+    freetype
+    glib
+    gsettings-desktop-schemas
+    gtk3
+    jdk
+    libarchive
+    libX11
+    libXrender
+    libXtst
+    libsecret
+    zlib
+    webkitgtk
+  ];
 
   buildCommand = ''
-  # Extract download
-  mkdir -p $out/eclipse
-  bsdtar -xf $src -C $out/eclipse
+    # Extract download
+    mkdir -p $out/DataFlowAnalysisBench
+    bsdtar -xf $src -C $out/DataFlowAnalysisBench/
 
-  # Patch binaries
-  interpreter="$(cat $NIX_BINTOOLS/nix-support/dynamic-linker)"
-    libCairo=$out/eclipse/libcairo-swt.so
-    patchelf --set-interpreter $interpreter $out/eclipse/eclipse
-    [ -f $libCairo ] && patchelf --set-rpath ${lib.makeLibraryPath [ freetype fontconfig libX11 libXrender zlib ]} $libCairo
+    # Patch binaries
+    interpreter="$(cat $NIX_BINTOOLS/nix-support/dynamic-linker)"
+      libCairo=$out/DataFlowAnalysisBench/libcairo-swt.so
+      patchelf --set-interpreter $interpreter $out/DataFlowAnalysisBench/DataFlowAnalysisBench
+      [ -f $libCairo ] && patchelf --set-rpath ${lib.makeLibraryPath [freetype fontconfig libX11 libXrender zlib]} $libCairo
 
-  # Create wrapper script
-  makeWrapper $out/eclipse/eclipse $out/bin/eclipse \
-      --prefix PATH : ${jdk}/bin \
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath ([ glib gtk3 libXtst libsecret ] ++ lib.optional (webkitgtk != null) webkitgtk)} \
-      --prefix GIO_EXTRA_MODULES : "${glib-networking}/lib/gio/modules" \
-      --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+    # Create wrapper script
+    productId=$(sed 's/id=//; t; d' $out/DataFlowAnalysisBench/.eclipseproduct)
+    makeWrapper $out/DataFlowAnalysisBench/DataFlowAnalysisBench $out/bin/DataFlowAnalysisBench \
+        --prefix PATH : ${jdk}/bin \
+        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [glib gtk3 libXtst libsecret webkitgtk]} \
+        --prefix GIO_EXTRA_MODULES : "${glib-networking}/lib/gio/modules" \
+        --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH" \
+        --add-flags "-configuration \$HOME/.eclipse/''${productId}_${version}/configuration"
 
-  # Create desktop item
-  mkdir -p $out/share/applications
-  cp ${desktopItem}/share/applications/* $out/share/applications 
+    # Create desktop item
+    mkdir -p $out/share/applications
+    cp ${desktopItem}/share/applications/* $out/share/applications
+    mkdir -p $out/share/pixmaps
+    ln -s $out/DataFlowAnalysisBench/icon.xpm $out/share/pixmaps/DataFlowAnalysisBench.xpm
+
+    # ensure eclipse.ini does not try to use a justj jvm, as those aren't compatible with nix
+    perl -i -p0e 's|-vm\nplugins/org.eclipse.justj.*/jre/bin.*\n||' $out/DataFlowAnalysisBench/DataFlowAnalysisBench.ini
   '';
 }
