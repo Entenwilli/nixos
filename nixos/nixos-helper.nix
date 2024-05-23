dc92abd9e15c4dfc14dbaa9937fc32c31a5f3120
Felix Schwickerath <felix@fschwickerath.de>
2024-05-23 18:58:09 +0200

    feat(nh): use nixos helper configuration options

--- nixos/nixos-helper.nix
+++ nixos/nixos-helper.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  pkgs,
   config,
   ...
 }: {
@@ -9,15 +8,11 @@
   };
 
   config = lib.mkIf config.nixos-helper.enable {
-    environment.systemPackages = with pkgs; [unstable.nh];
-    /*
-    TODO: Add when nh is in stable
     programs.nh = {
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 4d --keep 3";
       flake = "/home/felix/nixos";
     };
-    */
   };
 }
