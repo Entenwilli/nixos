29de627b7aa98fa0dc647f46162da975da072554
Felix Schwickerath <felix@fschwickerath.de>
2025-06-02 15:05:13 +0200

    feat(nixpkgs): update to 25.05

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Hardware configuration
