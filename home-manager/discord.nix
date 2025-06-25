e5b792bdf865dc4899aa1cd12c5da701319e9a1b
Felix Schwickerath <felix@fschwickerath.de>
2025-06-25 15:22:15 +0200

    feat(discord): use vencord instead of webcord

--- home-manager/discord.nix
+++ home-manager/discord.nix
@@ -11,15 +11,11 @@
   };
 
   config = lib.mkIf config.discord.enable {
-    home.packages = with pkgs; [webcord];
-    xdg.desktopEntries.webcord = {
-      name = "Discord";
-      genericName = "Discord";
-      exec = "webcord --enable-features=UseOzonePlatform --ozone-platform=wayland";
-      icon = "webcord";
-      terminal = false;
-      type = "Application";
-      categories = ["Network"];
-    };
+    home.packages = with pkgs; [
+      (discord.override {
+        withOpenASAR = true;
+        withVencord = true;
+      })
+    ];
   };
 }
