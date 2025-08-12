688f0a9a9415e8833b5ac02d0075593fe22cf4ff
Felix Schwickerath <felix@fschwickerath.de>
2025-08-12 07:58:12 +0200

    feat(notifications): add bedtime notification

--- nixos/notifications.nix
+++ nixos/notifications.nix
@@ -26,4 +26,18 @@
     };
     wantedBy = ["late.timer"];
   };
+
+  systemd.user.timers."bedtime" = {
+    timerConfig = {
+      OnCalendar = "*-*-* 22:00:00";
+    };
+    wantedBy = ["timers.target"];
+  };
+  systemd.user.services."bedtime" = {
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.dunst}/bin/dunstify --hints=\"string:fgcolor:#ff4444\" --appname=\"It\'s time to go to bed!\" --urgency=critical --timeout=0 --icon=\"face-tired\" \"It's 10pm... You should wrap up and begin going to bed!\"";
+    };
+    wantedBy = ["bedtime.timer"];
+  };
 }
