a53b84c26ac400900500ad7b6dd6cbe54d51276a
Felix Schwickerath <felix@fschwickerath.de>
2025-08-20 10:43:43 +0200

    chore: update packages

--- home-manager/homes/desktop.nix
+++ home-manager/homes/desktop.nix
@@ -102,10 +102,6 @@
   ];
   hyprland.hyprpaper.enable = false;
   hyprland.keyboardLayout = "us";
-  # Enable noisetorch on startup
-  hyprland.additional_config = ''
-    exec-once = noisetorch -i "alsa_input.usb-M-Audio_M-Track_2X2-00.analog-stereo"
-  '';
 
   # Enable starship promt
   starship.enable = true;
--- nixos/configurations/desktop.nix
+++ nixos/configurations/desktop.nix
@@ -119,6 +119,13 @@
   ];
 
   programs.noisetorch.enable = true;
+  systemd.user.services.noisetorch = {
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i 'alsa_input.usb-M-Audio_M-Track_2X2-00.analog-stereo'";
+    };
+    wantedBy = ["graphical-session.target"];
+  };
 
   # Setup syncthing
   services = {
