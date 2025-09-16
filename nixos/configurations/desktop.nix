d5e7942eab4485872b35e4058229b58cc7ce439c
Felix Schwickerath <felix@fschwickerath.de>
2025-09-16 20:02:13 +0200

    fix(openrgb): fix loading of profiles on startup

--- nixos/configurations/desktop.nix
+++ nixos/configurations/desktop.nix
@@ -4,6 +4,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
@@ -165,6 +166,19 @@
   services.hardware.openrgb = {
     enable = true;
     motherboard = "amd";
+  };
+
+  systemd.services.openrgb = {
+    description = "OpenRGB server daemon";
+    after = ["network.target"];
+    wants = ["dev-usb.device"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      StateDirectory = "OpenRGB";
+      WorkingDirectory = "/var/lib/OpenRGB";
+      ExecStart = lib.mkForce "${pkgs.openrgb}/bin/openrgb --server --server-port 6742 --profile Default.orp";
+      Restart = "always";
+    };
   };
 
   systemd.packages = with pkgs; [lact];
