48f1b9f81cfa861651f8f3fe127d17b45efa4c4d
Felix Schwickerath <felix@fschwickerath.de>
2025-11-11 10:04:09 +0100

    fix(notifications): fix erroneous fireing on startup

--- nixos/notifications.nix
+++ nixos/notifications.nix
@@ -2,6 +2,7 @@
   systemd.user.timers."break" = {
     timerConfig = {
       OnBootSec = "4h";
+      Unit = ["break.service"];
     };
     wantedBy = ["timers.target"];
   };
@@ -10,12 +11,12 @@
       Type = "simple";
       ExecStart = "${pkgs.dunst}/bin/dunstify --hints=\"string:fgcolor:#ff4444\" --appname=\"Time to take a Break!\" --urgency=critical --timeout=0 --icon=\"face-tired\" \"Over 4 hours have elapsed! Did you take a break?\"";
     };
-    wantedBy = ["break.timer"];
   };
 
   systemd.user.timers."late" = {
     timerConfig = {
       OnCalendar = "*-*-* 18:00:00";
+      Unit = ["late.service"];
     };
     wantedBy = ["timers.target"];
   };
@@ -24,12 +25,12 @@
       Type = "simple";
       ExecStart = "${pkgs.dunst}/bin/dunstify --hints=\"string:fgcolor:#ff4444\" --appname=\"It\'s getting late!\" --urgency=critical --timeout=0 --icon=\"face-tired\" \"It's 6pm... You should finish work for today!\"";
     };
-    wantedBy = ["late.timer"];
   };
 
   systemd.user.timers."bedtime" = {
     timerConfig = {
       OnCalendar = "*-*-* 22:00:00";
+      Unit = ["break.service"];
     };
     wantedBy = ["timers.target"];
   };
@@ -38,6 +39,5 @@
       Type = "simple";
       ExecStart = "${pkgs.dunst}/bin/dunstify --hints=\"string:fgcolor:#ff4444\" --appname=\"It\'s time to go to bed!\" --urgency=critical --timeout=0 --icon=\"face-tired\" \"It's 10pm... You should wrap up and begin going to bed!\"";
     };
-    wantedBy = ["bedtime.timer"];
   };
 }
