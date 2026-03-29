91c36458243f47092e74dd24e98c271d0201731c
Felix Schwickerath <felix@fschwickerath.de>
2026-03-29 12:56:49 +0200

    refactor(openrgb): update openrgb

--- nixos/openrgb.nix
+++ nixos/openrgb.nix
@@ -1,23 +1,7 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   services.hardware.openrgb = {
     enable = true;
     motherboard = "amd";
-  };
-
-  systemd.services.openrgb = {
-    description = "OpenRGB server daemon";
-    after = ["network.target"];
-    wants = ["dev-usb.device"];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      StateDirectory = "OpenRGB";
-      WorkingDirectory = "/var/lib/OpenRGB";
-      ExecStart = lib.mkForce "${pkgs.openrgb}/bin/openrgb --server --profile Default.orp";
-      Restart = "always";
-    };
+    startupProfile = "Default.orp";
   };
 }
