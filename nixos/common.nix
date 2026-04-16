f5eb268a081be038c8fe099106020dfe4d629e71
Felix Schwickerath <felix@fschwickerath.de>
2026-04-16 19:01:33 +0200

    fix(pipewire): fix audio crackling

--- nixos/common.nix
+++ nixos/common.nix
@@ -201,6 +201,14 @@
     alsa.enable = true;
     pulse.enable = true;
   };
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 128;
+      "default.clock.min-quantum" = 128;
+      "default.clock.max-quantum" = 128;
+    };
+  };
 
   # Installing Fonts
   fonts.packages = with pkgs; [
