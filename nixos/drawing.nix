f19dab1fb0fc54badb1ee7ff51c57a25d8436eda
Felix Schwickerath <felix@fschwickerath.de>
2026-06-03 20:59:22 +0200

    feat(csp): add csp prewarm service

--- nixos/drawing.nix
+++ nixos/drawing.nix
@@ -11,4 +11,16 @@
     enable = true;
     daemon.enable = true;
   };
+
+  systemd.user.services.csp-prewarm = {
+    serviceConfig = {
+      Type = "simple";
+      Environment = "WINEPREFIX=/home/felix/.wine-csp/";
+      ExecStartPre = "-${pkgs.wine64Packages.unstableFull}/bin/wineserver -k";
+      ExecStart = "${pkgs.wine64Packages.unstableFull}/bin/wineserver -f";
+      Restart = "always";
+    };
+    wantedBy = ["default.target"];
+    after = ["default.target"];
+  };
 }
