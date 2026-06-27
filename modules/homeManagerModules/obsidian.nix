b7940b5c07d9360119851eef00071ed41a80b8d6
Felix Schwickerath <felix@fschwickerath.de>
2026-06-27 15:28:02 +0200

    fix(obsidian): remove obisidan command line options

--- modules/homeManagerModules/obsidian.nix
+++ modules/homeManagerModules/obsidian.nix
@@ -6,7 +6,7 @@
   }: {
     xdg.desktopEntries.obsidian = {
       name = "Obsidian";
-      exec = "${pkgs.obsidian}/bin/obsidian --use-vulkan --use-gl=egl --enable-zero-copy --enable-hardware-overlays --enable-features=VaapiVideoDecoder,VaapiVideoEncoder,CanvasOopRasterization,VaapiIgnoreDriverChecks --disable-features=UseSkiaRenderer,UseChromeOSDirectVideoDecoder --ignore-gpu-blocklist %u";
+      exec = "${pkgs.obsidian}/bin/obsidian %u";
       icon = "obsidian";
       mimeType = ["x-scheme-handler/obsidian"];
       type = "Application";
